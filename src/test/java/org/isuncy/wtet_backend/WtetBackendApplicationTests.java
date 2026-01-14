acd66820198d9aac022852887802110ff32f0551
isuncy <3524654439@qq.com>
2026-01-15 02:50:31 +0800

    创建项目

---
+++ src/test/java/org/isuncy/wtet_backend/WtetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.isuncy.wtet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
